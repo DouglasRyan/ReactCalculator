4d3a11f4db65b0e9355daefe3bc05ff6abb3fbbb
ryan <2869888186@qq.com>
2019-06-06 07:11:13 +0800

    添加homepage

---
+++ build/precache-manifest.77736937ecf75e7c3df2d43decf09ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416fcd02b1ef325a94d2a49cc7a1aa79",
+    "url": "/ReactCalculator/build/index.html"
+  },
+  {
+    "revision": "98175b435b660ed5561c",
+    "url": "/ReactCalculator/build/static/css/main.28319404.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/ReactCalculator/build/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "98175b435b660ed5561c",
+    "url": "/ReactCalculator/build/static/js/main.53c9491b.chunk.js"
+  },
+  {
+    "revision": "357f4a0462c787820574",
+    "url": "/ReactCalculator/build/static/js/runtime~main.a25f593c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fb09fdcac0078c4c99b146064e3da23.js"
+  "/ReactCalculator/build/precache-manifest.77736937ecf75e7c3df2d43decf09ba8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactCalculator/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
